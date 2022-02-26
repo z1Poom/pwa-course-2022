f53a764a5b15caab7c99bfda148f9afc7974250a
z1Poom <Judges190@gmail.com>
2022-02-26 17:30:30 +0700

    workclass 07/ex01

--- src/FriendList.js
+++ src/FriendList.js
@@ -14,7 +14,14 @@ function FriendList(){
   return(
     <App>
       <div className="friend-list">
-       ใส่โค้ดที่นี่
+       {
+         friends.map ( x=> 
+          <div className = 'friend-list-item'> 
+            <Avatar name = {x.user} /> 
+            {x.displayName}
+            </div>
+            )
+       } 
       </div>
     </App>
   )
